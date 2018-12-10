143bbe13a038df8a30d304bb6248b5c9e6297334
Daniel Sturm <d.c.sturm@gmail.com>
2018-12-10 12:06:34 +0100

    Add ability to check IP and make PSR2 compliant.

--- webhook.php
+++ webhook.php
@@ -1,25 +1,57 @@
 <?php
+/**
+ * File webhook.php
+ * 
+ * @author Daniel Sturm <d.sturm@zirkeldesign.de>
+ */
+
 require_once __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\HttpFoundation\IpUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 if (!file_exists(__DIR__.'/config.yml')) {
     echo "Please, define your satis configuration in a config.yml file.\nYou can use the config.yml.dist as a template.";
     exit(-1);
 }
 
-$defaults = array(
+$request = Request::createFromGlobals();
+
+$defaults = [
     'bin' => 'bin/satis',
     'json' => 'satis.json',
     'webroot' => 'web/',
     'user' => null,
-    'secret' => null
-);
+    'secret' => null,
+    'authorized_ips' => null
+];
 $config = Yaml::parse(__DIR__.'/config.yml');
 $config = array_merge($defaults, $config);
 
-$errors = array();
+if (null !== $config['authorized_ips']) {
+    $ip = $request->getClientIp();
+    $authorized = false;
+    if (is_array($config['authorized_ips'])) {
+        foreach ($config['authorized_ips'] as $authorizedIp) {
+            $authorized = IpUtils::checkIp($ip, $authorizedIp);
+            if ($authorized) {
+                break;
+            }
+        }
+    } else {
+        $authorized = IpUtils::checkIp($ip, $config['authorized_ips']);
+    }
+    if (! $authorized) {
+        http_response_code(403);
+        exit(-1);
+    }
+}
+
+$errors = [];
 if (!file_exists($config['bin'])) {
     $errors[] = 'The Satis bin could not be found.';
 }
@@ -43,61 +75,58 @@ if (!empty($errors)) {
 // Determine GIT provider
 if (key_exists('HTTP_X_GITHUB_EVENT', $_SERVER)) {
     $gitProvider = 'github';
-}
-elseif ((key_exists('HTTP_X_GITLAB_EVENT', $_SERVER))) {
+} elseif ((key_exists('HTTP_X_GITLAB_EVENT', $_SERVER))) {
     $gitProvider = 'gitlab';
-}
-else {
+} else {
     $gitProvider = 'general';
 }
 
 switch ($gitProvider) {
+case 'github':
+    // Read the JSON data from GitHub
+    $rawPost = file_get_contents('php://input');
+    $githubData = json_decode($rawPost);
 
-    case 'github':
-        // Read the JSON data from GitHub
-        $rawPost = file_get_contents('php://input');
-        $githubData = json_decode($rawPost);
-
-        // Check the secret
-        $header = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-        list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-        try {
-            if ($hash !== hash_hmac($algo, $rawPost, $config['secret'])) {
-                throw new \Exception('Hook secret does not match.');
-            }
+    // Check the secret
+    $header = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + ['', ''];
+    try {
+        if ($hash !== hash_hmac($algo, $rawPost, $config['secret'])) {
+            throw new \Exception('Hook secret does not match.');
         }
-        catch (Exception $e) {
-            header('HTTP/1.1 403 Forbidden');
-            echo $e->getMessage();die;
+    }
+    catch (Exception $e) {
+        header('HTTP/1.1 403 Forbidden');
+        echo $e->getMessage();die;
+    }
+
+    // Get the repo URL's
+    $cloneUrl = $githubData->repository->clone_url;
+    $sshUrl = $githubData->repository->ssh_url;
+    break;
+
+case 'gitlab':
+    // Read the JSON data from GitLab
+    $rawPost = file_get_contents('php://input');
+    $gitlabData = json_decode($rawPost);
+
+    $receivedSecret = $_SERVER['HTTP_X_GITLAB_TOKEN'];
+    try {
+        if ($receivedSecret !== $config['secret']) {
+            throw new \Exception('Hook secret does not match.');
         }
+    }
+    catch (Exception $e) {
+        header('HTTP/1.1 403 Forbidden');
+        echo $e->getMessage();die;
+    }
 
-        // Get the repo URL's
-        $cloneUrl = $githubData->repository->clone_url;
-        $sshUrl = $githubData->repository->ssh_url;
-        break;
+    // Get the repo URL's
+    $cloneUrl = $gitlabData->project->http_url;
+    $sshUrl = $gitlabData->project->ssh_url;
+    break;
 
-    case 'gitlab':
-        // Read the JSON data from GitLab
-        $rawPost = file_get_contents('php://input');
-        $gitlabData = json_decode($rawPost);
-
-        $receivedSecret = $_SERVER['HTTP_X_GITLAB_TOKEN'];
-        try {
-            if ($receivedSecret !== $config['secret']) {
-                throw new \Exception('Hook secret does not match.');
-            }
-        }
-        catch (Exception $e) {
-            header('HTTP/1.1 403 Forbidden');
-            echo $e->getMessage();die;
-        }
-
-        // Get the repo URL's
-        $cloneUrl = $gitlabData->project->http_url;
-        $sshUrl = $gitlabData->project->ssh_url;
-        break;
-
-    default:
+default:
 }
 
 // Read the satis JSON
@@ -105,33 +134,49 @@ $satisData = json_decode(file_get_contents('satis.json'));
 
 $command = null;
 
-if ($gitProvider === 'github' || $gitProvider === 'gitlab') {
+if (in_array($gitProvider, ['github', 'gitlab'], true)) {
     // Update a specific repository if the webhook call came from GitHub or GitLab
-    foreach($satisData->repositories as $repository) {
-        if ($repository->url === $cloneUrl || $repository->url === $sshUrl) {
+    foreach ($satisData->repositories as $repository) {
+        if (in_array($repository->url, [$cloneUrl, $sshUrl], true)) {
             $repositoryUrl = $repository->url;
         }
     }
 
-    $command = sprintf('%s build --repository-url %s %s %s', $config['bin'], $repositoryUrl, $config['json'], $config['webroot']);
-}
-else {
+    $command = [
+        $config['bin'],
+        'build',
+        '--repository-url',
+        $repositoryUrl,
+        $config['json'],
+        $config['webroot'],
+    ];
+} else {
     // Update all repositories if the webhook didn't come from GitHub or GitLab
-    $command = sprintf('%s build %s %s', $config['bin'], $config['json'], $config['webroot']);
+    $command = [
+        $config['bin'],
+        'build',
+        $repositoryUrl,
+        $config['json'],
+        $config['webroot'],
+    ];
 }
 
-if (null !== $config['user'] && !is_null($command)) {
+if (null !== $config['user']
+    && !is_null($command)
+) {
     $command = sprintf('sudo -u %s -i %s', $config['user'], $command);
 }
 
 $process = new Process($command);
-$exitCode = $process->run(function ($type, $buffer) {
-    if ('err' === $type) {
-        echo 'E';
-        error_log($buffer);
-    } else {
-        echo '.';
+$exitCode = $process->run(
+    function ($type, $buffer) {
+        if (Process::ERR === $type) {
+            echo 'E';
+            error_log($buffer);
+        } else {
+            echo '.';
+        }
     }
-});
+);
 
 echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild!' : 'Oops! An error occured!') . "\n";
